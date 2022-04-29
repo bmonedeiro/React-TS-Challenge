b2dc3ccf5c9dc069dc9803923f25622d6136b573
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-04-29 20:32:44 -0300

    feat: movie service

---
+++ src/services/movie.test.ts
@@ -0,0 +1,20 @@
+import MockAdapter from 'axios-mock-adapter';
+
+import { getMovie } from './movie';
+import { movieMockResponse } from '../mocks'
+import api from './api';
+
+const apiMock = new MockAdapter(api);
+
+describe('services/movie', () => {
+  it('should getMovie with the appropriate response', async () => {
+    apiMock
+      .onGet(`movie/${movieMockResponse.id}`)
+      .reply(200, movieMockResponse);
+
+    const response = await getMovie(movieMockResponse.id);
+
+    expect(response).toEqual(movieMockResponse);
+  });
+
+});
---
+++ src/services/movie.ts
@@ -0,0 +1,14 @@
+import { AxiosError } from 'axios'
+
+import api from './api'
+import { MovieDetail } from '../types/movie'
+
+export const getMovie = async(movieId: number) => {
+  try {
+    const { data } = await api.get<MovieDetail>(`movie/${movieId}`)
+    return data
+  } catch (err) {
+    const error = err as Error | AxiosError;
+    throw(error?.message)
+  }
+}
