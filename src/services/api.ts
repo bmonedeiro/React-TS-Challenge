fb350b41b7d62c22016aeb43bb6f60928cc1e9e2
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-04-29 12:06:47 -0300

    feat: adds dotenv and create default axios instance api

---
+++ src/services/api.ts
@@ -0,0 +1,12 @@
+import axios from 'axios'
+
+const BASE_URL= 'https://api.themoviedb.org/3/'
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  params: {
+    api_key: process.env.API_KEY,
+  }
+})
+
+export default api
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
-  output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-  mode: process.env.NODE_ENV || "development",
-  resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-  devServer: { static: path.join(__dirname, "src") },
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js' },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
+  devServer: { static: path.join(__dirname, 'src') },
   plugins: [
     new HtmlWebpackPlugin({
-        template: path.join(__dirname, "public", "index.html"),
+      template: path.join(__dirname, 'public', 'index.html')
     }),
+    new Dotenv()
   ],
   module: {
     rules: [
-        { 
-            test: /\.(js|jsx)$/, 
-            exclude: /node_modules/, 
-            use: ["babel-loader"] 
-        },
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /node_modules/,
-          use: ["ts-loader"],
-        },
-        {
-            test: /\.(css|scss)$/,
-            use: ["style-loader", "css-loader"],
-        },
-        { 
-            test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-            use: ["file-loader"],
-        },
-    ],
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        use: ['ts-loader']
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+        use: ['file-loader']
+      }
+    ]
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js']
   }
-};
+}
