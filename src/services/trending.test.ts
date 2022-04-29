4a4a7242ac1588f42d002ddaaa69dc1ccc0de4b2
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-04-29 18:41:42 -0300

    feat: update mock data

--- src/services/trending.test.ts
+++ src/services/trending.test.ts
@@ -1,47 +1,20 @@
 import MockAdapter from 'axios-mock-adapter';
 
 import { getTrending } from './trending';
+import { trendResponseMock } from '../mocks'
 import api from './api';
 
 const apiMock = new MockAdapter(api);
-const mockData = {
-  "page": 1,
-  "results": [
-    {
-      "adult": false,
-      "backdrop_path": "/bOGkgRGdhrBYJSLpXaxhXVstddV.jpg",
-      "genre_ids": [
-        28,
-        12,
-        14,
-        878
-      ],
-      "id": 299536,
-      "original_language": "en",
-      "original_title": "Avengers: Infinity War",
-      "overview": "As the Avengers and their allies have continued to protect the world from threats too large for any one hero to handle, a new danger has emerged from the cosmic shadows: Thanos. A despot of intergalactic infamy, his goal is to collect all six Infinity Stones, artifacts of unimaginable power, and use them to inflict his twisted will on all of reality. Everything the Avengers have fought for has led up to this moment - the fate of Earth and existence itself has never been more uncertain.",
-      "poster_path": "/7WsyChQLEftFiDOVTGkv3hFpyyt.jpg",
-      "release_date": "2018-04-25",
-      "title": "Avengers: Infinity War",
-      "video": false,
-      "vote_average": 8.3,
-      "vote_count": 6937,
-      "popularity": 358.799
-    },
-  ],
-  "total_pages": 792,
-  "total_results": 15831
-}
 
 describe('services/trending', () => {
   it('should getTrending with the appropriate response', async () => {
     apiMock
       .onGet('trending/all/week')
-      .reply(200, mockData);
+      .reply(200, trendResponseMock);
 
     const { results } = await getTrending();
 
-    expect(results).toEqual(mockData.results);
+    expect(results).toEqual(trendResponseMock.results);
   });
 
 });
