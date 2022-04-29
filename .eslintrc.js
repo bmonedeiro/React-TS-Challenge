4b084a4658d5b18862be2cccf5f8d70b9afab0dd
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-04-29 11:11:09 -0300

    chore: lint config

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+export default {
+    'env': {
+        'browser': true,
+        'es2021': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'airbnb',
+        'prettier',
+        'react-app',
+        'plugin:prettier/recommended',
+    ],
+    'parser': '@typescript-eslint/parser',
+    'parserOptions': {
+        'ecmaFeatures': {
+            'jsx': true
+        },
+        'ecmaVersion': 'latest',
+        'sourceType': 'module'
+    },
+    'plugins': [
+        'react',
+        '@typescript-eslint',
+        'prettier',
+        'react-hooks',
+        'import',
+        'jsx-a11y'
+    ]
+}
