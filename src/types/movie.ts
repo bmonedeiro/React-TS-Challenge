782b5a049578ca778ae611da4831cd19b1e6a9c3
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-04-29 20:33:00 -0300

    feat: movie type

---
+++ src/types/movie.ts
@@ -0,0 +1,58 @@
+export type MovieDetail = {
+  adult: boolean
+  backdrop_path: string | null
+  belongs_to_collection: BelongsToCollection | null
+  budget: number
+  genre: Array<Genre>
+  homepage: string | null
+  id: number
+  imdb_id: string | null
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  production_companies: Array<ProductionCompany>
+  production_countries: Array<ProductionCountry>
+  release_date: string
+  revenue: number
+  runtime: number | null
+  spoken_languages: Array<Language>
+  status: StatusValues
+  tagline: string | null
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+type BelongsToCollection = {
+  id: number
+  name: string
+  poster_path: string
+  backdrop_path: string
+}
+
+type Genre = {
+  id: number
+  name: string
+}
+
+type ProductionCompany = {
+  id: number
+  logo_path: string | null
+  name: string
+  origin_country: string
+}
+
+type ProductionCountry = {
+  iso_3166_1: string
+  name: string
+}
+
+type Language = {
+  iso_639_1: string
+  name: string
+}
+
+type StatusValues = 'Rumored' | 'Planned' | 'In Production' | 'Post Production' | 'Released' | 'Canceled'
