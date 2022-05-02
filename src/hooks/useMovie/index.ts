76124d322b4224f730b3a527eae0b7ab32b85e48
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-05-01 22:56:48 -0300

    feat: formatter movie data

--- src/hooks/useMovie/index.ts
+++ src/hooks/useMovie/index.ts
@@ -1,10 +1,29 @@
 import { useEffect, useState } from "react"
 
 import { getMovie } from '@src/services/movie'
-import { MovieDetail } from "@src/types/movie"
+import { MovieDetail, MovieProfile } from "@src/types/movie"
+import currencyFormatter from "@src/utils/currencyFormatter"
+
+const formatterMovieData = (movie: MovieDetail): MovieProfile => {
+  const formattedGenre = movie.genres.map((genre) => genre.name).join(', ')
+  const productionCompaniesName = movie.production_companies.map((company) => company.name).join(', ')
+
+  return [
+    { name: 'Genre', value: formattedGenre, type: 'string' },
+    { name: 'Overview', value: movie.overview, type: 'string' },
+    { name: 'Production Companies Name', value: productionCompaniesName, type: 'string' },
+    { name: 'Release Date', value: movie.release_date, type: 'string' },
+    { name: 'Vote', value: `${movie.vote_average}(${movie.vote_count})`, type: 'string' },
+    { name: 'IMDB ID', value: (movie.imdb_id) || '', type: 'string' },
+    { name: 'Revenue', value: currencyFormatter.format(movie.revenue), type: 'string' },
+    { name: 'Title', value: movie.title, type: 'string' },
+    { name: 'Homepage', value: movie.homepage || '', type: 'link' },
+    { name: 'Poster', value: movie.poster_path || '', type: 'img' },
+  ]
+}
 
 const useMovie = (movieId: number | null) => {
-  const [movie, setMovie] = useState<MovieDetail>()
+  const [movie, setMovie] = useState<MovieProfile>()
   const [error, setError] = useState<Error | null>(null)
   const [isFetching, setIsFetching] = useState<boolean>(false)
 
@@ -14,7 +33,7 @@ const useMovie = (movieId: number | null) => {
       try {
         setIsFetching(true)
         const response = await getMovie(movieId)
-        setMovie(response)
+        setMovie(formatterMovieData(response))
       } catch(e) {
         setError(e as Error)
       } finally {
