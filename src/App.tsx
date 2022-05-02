bcb4e0a8ec25e4de107fea8e5c7b9df8098ffd4e
Bárbara Monedeiro <bmonedeiro@gmail.com>
2022-05-01 21:48:13 -0300

    feat: error page

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,26 @@
 import { Component} from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Error from "./pages/Error";
 import "./global.css"
-
-/**
- * The starting page for your App
- */
-
 class App extends Component{
   render(){
     return(
-      <>
-        <BrowserRouter>
-          <Header />
-          <main>
-            <section>
-                <Routes>
-                  <Route path={"/"} element={<Home />} />
-                  <Route path="/about/:movieId" element={<About />} />
-                  <Route
-                    path="*"
-                    element={
-                      <main style={{ padding: "1rem" }}>
-                        <p>There's nothing here!</p>
-                      </main>
-                    }
-                  />
-                </Routes>
-            </section>
-          </main>
-        </BrowserRouter>
-      </>
+      <BrowserRouter>
+        <main>
+          <section>
+            <Routes>
+              <Route path={"/"} element={<Home />} />
+              <Route path="/about/:movieId" element={<About />} />
+              <Route
+                path="*"
+                element={<Error />}
+              />
+            </Routes>
+          </section>
+        </main>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/pages/Error.tsx
@@ -0,0 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
+import ExclamationIcon from "@src/components/Icons/ExclamationIcon";
+import ArrowRightIcon from '@src/components/Icons/ArrowRightIcon';
+
+interface ErrorPageProps {
+  errorCode?: string
+  title?: string
+  description?: string
+}
+
+export const Error = ({
+  errorCode = "404 error",
+  title = "Page not found.",
+  description = "Sorry, we couldn't find the page you're looking for"
+}: ErrorPageProps) => {
+  const navigate = useNavigate();
+  return (
+    <div className="h-screen w-screen flex flex-col justify-center items-center space-y-4">
+      <ExclamationIcon />
+      <h3 className="text-primary text-sm uppercase font-bold tracking-widest">{errorCode}</h3>
+      <h1 className="text-secondary text-4xl font-bold">{title}</h1>
+      <p className="text-gray-500 font-light">{description}</p>
+      <button type="button" onClick={() => { navigate("/")}} className="flex space-x-2 items-center text-primary">
+        <span>Go back home</span>
+        <ArrowRightIcon />
+      </button>
+    </div>
+  );
+};
+
+export default Error;
